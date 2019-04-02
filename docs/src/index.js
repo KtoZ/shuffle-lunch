2746ea685da4b44e5c0f575a49982c2a2586a95c
KtoZ <32612782+KtoZ@users.noreply.github.com>
2019-04-03 02:25:51 +0900

    Deploy

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,4 +1,46 @@
-function shuffle(array) {
+function shuffle() {
+  let results = document.querySelector("#results");
+  results.innerHTML = "";
+
+  let members = document
+    .querySelector("#members")
+    .value.replace(/\r?\n/g, "")
+    .split(",");
+  let s = shuffleArray(members);
+  let group = getGroup(s);
+
+  for (var i = 0; i < group.length; i++) {
+    var col = document.createElement("div");
+    col.className = "col s12 m6";
+    results.appendChild(col);
+
+    var ul = document.createElement("ul");
+    ul.className = "collection with-header";
+    col.appendChild(ul);
+
+    var color = getColor();
+    var liHeader = document.createElement("li");
+    liHeader.className = `collection-item lighten-4 ${color}`;
+    ul.appendChild(liHeader);
+
+    var h4 = document.createElement("h4");
+    h4.textContent = "Group " + (i + 1);
+    liHeader.appendChild(h4);
+
+    for (var j = 0; j < group[i].length; j++) {
+      var li = document.createElement("li");
+      li.className = "collection-item lighten-5 gray";
+      li.textContent = group[i][j];
+      ul.appendChild(li);
+    }
+  }
+
+  let logItem = toLogItem(group);
+  addLog(logItem);
+  dispLog();
+}
+
+function shuffleArray(array) {
   var n = array.length,
     t,
     i;
@@ -55,3 +97,96 @@ function getGroup(array) {
 
   return group;
 }
+
+function getColor() {
+  const colors = [
+    "red",
+    "pink",
+    "purple",
+    "blue",
+    "cyan",
+    "teal",
+    "green",
+    "yellow",
+    "amber",
+    "orange"
+  ];
+  return shuffleArray(colors).shift();
+}
+
+function dispLog() {
+  let tbody = document.querySelector("#log tbody");
+  tbody.innerHTML = "";
+
+  var log = getLog();
+  log.forEach(logItem => {
+    let tr = document.createElement('tr');
+    tr.id = logItem.id;
+
+    let td1 = document.createElement('td');
+    td1.textContent = new Date(logItem.date).toLocaleDateString();
+    tr.appendChild(td1);
+
+    let td2 = document.createElement('td');
+    logItem.group.forEach(members => {
+      let p = document.createElement('p');
+      p.textContent = members;
+      td2.appendChild(p);
+    });
+    tr.appendChild(td2);
+
+    tbody.appendChild(tr);
+  });
+}
+
+function getLog() {
+  let storage = localStorage.getItem('log');
+  return storage ? JSON.parse(storage) : [];
+}
+
+function addLog(logItem) {
+  let log = getLog();
+  log.unshift(logItem);
+
+  if (log.length > 20) {
+    log.pop();
+  }
+
+  localStorage.setItem('log', JSON.stringify(log));
+}
+
+function toLogItem(groups) {
+  let logItem = {
+    id: generateUuid(),
+    date: new Date(Date.now()).toJSON(),
+    group: []
+  };
+
+  groups.forEach(group => {
+    logItem.group.push(group);
+  });
+
+  return logItem;
+}
+
+function generateUuid() {
+  // https://github.com/GoogleChrome/chrome-platform-analytics/blob/master/src/internal/identifier.js
+  // const FORMAT: string = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
+  let chars = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
+  for (let i = 0, len = chars.length; i < len; i++) {
+    switch (chars[i]) {
+      case "x":
+        chars[i] = Math.floor(Math.random() * 16).toString(16);
+        break;
+      case "y":
+        chars[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+        break;
+    }
+  }
+  return chars.join("");
+}
+
+
+// loaded
+document.querySelector("#shuffle").addEventListener('click', shuffle);
+window.addEventListener('load', dispLog);
